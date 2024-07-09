ee7eb57f7a50506b592f3fd192e3987f7286ca8c
nikita_ch <nikita_ch@maximail.org>
2024-07-09 16:37:17 +0500

    Миграция зависимостей до каталога версий

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-val kotlin_version: String by project
-val logback_version: String by project
-val exposed_version: String by project
-val postgresql_version: String by project
-val ktor_version: String by project
-
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("io.ktor.plugin") version "2.3.12"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 group = "ru.novbicreate"
@@ -25,25 +19,14 @@ repositories {
 }
 
 dependencies {
-    //Ktor
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-websockets:$ktor_version")
-
-    //PostgreSQL
-    implementation("org.postgresql:postgresql:$postgresql_version")
-
-    //Exposed
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-
-    //Logging
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-
-    //Test's
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.io.ktor.server.core)
+    implementation(libs.io.ktor.server.netty)
+    implementation(libs.io.ktor.server.websockets)
+    implementation(libs.io.ktor.server.content.negotiation)
+    implementation(libs.io.ktor.serialization.kotlinx.json)
+    implementation(libs.org.postgresql)
+    implementation(libs.org.jetbrains.exposed.core)
+    implementation(libs.org.jetbrains.exposed.dao)
+    implementation(libs.org.jetbrains.exposed.jdbc)
+    implementation(libs.ch.qos.logback)
 }
--- src/main/kotlin/ru/novbicreate/Application.kt
+++ src/main/kotlin/ru/novbicreate/Application.kt
@@ -4,7 +4,6 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.jetbrains.exposed.sql.Database
-import ru.novbicreate.controller.error.configureErrorRouting
 import ru.novbicreate.plugins.configureRouting
 import ru.novbicreate.plugins.configureSerialization
 import ru.novbicreate.plugins.configureWebSockets
